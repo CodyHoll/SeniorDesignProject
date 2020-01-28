97056868cc94aa53fb6fa1646e728793e353ae56
CodyHoll <cl7holland@gmail.com>
2020-01-28 15:40:45 -0600

    added authorization code for SAS and the node.js server that runs locally

--- Code/server.js
+++ Code/server.js
@@ -4,6 +4,6 @@ var http = require("http");
 //Create an HTTP server to handle responses
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world :)");
+    response.write("Hello world");
     response.end();
 }).listen(8888);
