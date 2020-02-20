c19f263bee0531832deaab99cab7cbd2e524cc59
CodyHoll <cl7holland@gmail.com>
2020-02-20 11:54:45 -0600

    THE WORKING PROTOTYPE (V1). TRY OUT THE GAME AND CLICK THE BUTTONS :).

--- Code/server.js
+++ Code/server.js
@@ -1,9 +1,73 @@
-// Load the HTTP library
-var http = require("http");
+// server.js
+// where your node app starts
 
-//Create an HTTP server to handle responses
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world");
-    response.end();
-}).listen(8888);
+// init project
+require('dotenv').config();
+var qs = require('querystring');
+var express = require('express');
+var app = express();
+
+// init Spotify API wrapper
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : process.env.CLIENT_ID,
+  clientSecret : process.env.CLIENT_SECRET,
+});
+
+const jssdkscopes = ["streaming", "user-read-email", "user-read-private", "user-modify-playback-state"];
+const redirectUriParameters = {
+  client_id: process.env.CLIENT_ID,
+  response_type: 'token',
+  scope: jssdkscopes.join(' '),
+  redirect_uri: encodeURI('http://localhost:8888/index.html'),
+  show_dialog: true,
+}
+
+const redirectUri = `https://accounts.spotify.com/authorize?${qs.stringify(redirectUriParameters)}`;
+
+function authenticate(callback) {
+  spotifyApi.clientCredentialsGrant()
+    .then(function(data) {
+      console.log('The access token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+    
+      callback instanceof Function && callback();
+
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    }, function(err) {
+      console.log('Something went wrong when retrieving an access token', err.message);
+    });
+}
+authenticate();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+app.get("/tracks", function(request, response){
+  reAuthenticateOnFailure((failure) => {
+    spotifyApi.getTrack(request.query.id)
+    .then(function(data){
+      response.send(data.body);
+    }, failure);
+  })
+});
+
+const reAuthenticateOnFailure = (action) => {
+  action(() => {
+    authenticate(action);
+  })
+}
+
+app.get("/spotifyRedirectUri", function (request, response) {
+  response.send(JSON.stringify({
+    redirectUri
+  }, null, 2))
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
